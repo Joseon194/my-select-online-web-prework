cf862b9164a83123832a11ab27864c0bac6f8b7d
Dennis Chung <chungdennis6@gmail.com>
2019-02-26 01:57:46 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,12 @@
 def my_select(collection)
- # your code here!
+new_array = []
+ i = 0
+ while i < collection.length
+ if yield collection[i]
+    new_array << collection[i]
+  else
+    nil
+  end
+  i += 1
+ end
 end
